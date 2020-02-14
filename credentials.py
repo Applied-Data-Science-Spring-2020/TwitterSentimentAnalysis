c0e88d70d308a668b67a7f59d532a06c1f31c672
Abhinav <48073517+bonnyabhinav@users.noreply.github.com>
2020-02-14 00:22:23 -0500

    credentials
    For authorization

---
+++ credentials.py
@@ -0,0 +1,5 @@
+#User Credentials that will be used to authorize the connection with the tweepy API
+ACCESS_TOKEN = "1213270339293077504-0VGZgQTrbUs3cNfITH8SkytxGuLmcE"
+ACCESS_TOKEN_SECRET = "zPqKpQumQkcKf52iIakDtjH6C6kbFN6M4w2H4im1yZ48a"
+CONSUMER_KEY = "GgMgpR0X0Bf3j6Y8UzeBY5eCp"
+CONSUMER_SECRET = "hA13Bslmb4Bi9Vo4WGwRKSVELiksYmGGOiH7lITvWnarS4HOMn"
